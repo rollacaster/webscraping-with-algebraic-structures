65952ad4bbbfa4ef18c75a360b5b588bd640d22c
Thomas Sojka <thomas.sojka@comsysto.com>
2019-12-07 18:14:27 +0100

    Update

--- scrape-fns.js
+++ scrape-fns.js
@@ -2,6 +2,9 @@ const _ = require('ramda')
 const { Future } = require('ramda-fantasy')
 const axios = require('axios')
 const cheerio = require('cheerio')
+const {
+  compareTwoStrings
+} = require('string-similarity')
 
 exports.loadPage = url =>
   Future((reject, resolve) =>
@@ -15,3 +18,6 @@ exports.loadPage = url =>
 
 exports.scrapePage = selector =>
   _.pipe(cheerio.load, $ => $(selector).get())
+
+exports.compareStrings = topic1 => topic2 =>
+  compareTwoStrings(topic1, topic2)
